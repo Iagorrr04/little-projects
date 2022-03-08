04a80eba41d45e4fb31ee91c914c4b3347d59948
iagorrr04 <85769349+Iagorrr04@users.noreply.github>
2022-03-08 10:21:24 -0300

    date error fixed

--- iClock/script.js
+++ iClock/script.js
@@ -1,43 +1,46 @@
 // Definition of our function.
 function showTime(){   
     // Receiving information of the pc.
-    let date = new Date()
+    let date = new Date();
     let hours = date.getHours(); // 0 - 23
     let minutes = date.getMinutes(); // 0 - 59
     let seconds = date.getSeconds(); // 0 - 59  
 
     // Case of zero
-    hours = (hours < 10) ? "0"+ hours : hours
-    minutes = (minutes < 10) ? "0" + minutes : minutes
-    seconds = (seconds < 10) ? "0" + seconds : seconds
+    hours = (hours < 10) ? "0"+ hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
     
     // Formated text to show.
     let fTime = hours + ':' + minutes + ':' + seconds;
 
     // Alterating the text inside our display div.
-    document.getElementById("display").innerText = fTime
-    document.getElementById("display").textContent = fTime
+    document.getElementById("display").innerText = fTime;
+    document.getElementById("display").textContent = fTime;
 }
 
 function showDate(){
     // Receiving the date.
     let date = new Date();
-    let day = date.getDay();
-    let month = date.getMonth();
+    let day = date.getDate();
+    let month = date.getMonth()+1;
     let year = date.getFullYear();
+    console.log(day);
+    console.log(month);
+    console.log(year);
     
     // Case of zeros.
-    day = (day < 10) ? "0"+day : day
-    month = (month < 10) ? "0"+month : month
+    day = (day < 10) ? ("0"+day) : day;
+    month = (month < 10) ? ("0"+month) : month;
 
     // Formated date to show.
-    let fDate = day + '/' + month + '/' + year
+    let fDate = day + '.' + month + '.' + year;
 
     //Alteranting the text to be showed.
-    document.getElementById("date").innerText = fDate
-    document.getElementById("date").textContent = fDate
+    document.getElementById("date").innerText = fDate;
+    document.getElementById("date").textContent = fDate;
 }
 
 // Calling our functions, at this point is just  a static clock.
 setInterval((showTime), 1000); // will repeat the function second by second.(not static anymore)
-setInterval(showDate,1000)
+setInterval(showDate,1000);
